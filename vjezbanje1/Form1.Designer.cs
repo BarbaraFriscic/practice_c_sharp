31d1a757f0d9e36961f34e28f2872b08e31f1be7
Barbara Friščić <80264267+BarbaraFriscic@users.noreply.github.com>
2022-10-07 10:20:36 +0200

    Add project files.

---
+++ vjezbanje1/Form1.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace vjezbanje1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRacunaj = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblRezultat = new System.Windows.Forms.Label();
+            this.txtPrviBroj = new System.Windows.Forms.TextBox();
+            this.txtDrugiBroj = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtStringUnos = new System.Windows.Forms.TextBox();
+            this.btnBrojacSlova = new System.Windows.Forms.Button();
+            this.lblRezultatPrebrojavanja = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblPrviSlucBr = new System.Windows.Forms.Label();
+            this.lblDrugiSlucBr = new System.Windows.Forms.Label();
+            this.btnSlucBr = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnSlucBrUsp = new System.Windows.Forms.Button();
+            this.lblRezUspSlucBr = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblPrviSluc = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblDrugiSluc = new System.Windows.Forms.Label();
+            this.btnPokreniSluc = new System.Windows.Forms.Button();
+            this.lblIspisZadatka = new System.Windows.Forms.Label();
+            this.btnPridruzivanje = new System.Windows.Forms.Button();
+            this.txtUnosBr = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnUsporedjivanje = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRacunaj
+            // 
+            this.btnRacunaj.Location = new System.Drawing.Point(544, 18);
+            this.btnRacunaj.Name = "btnRacunaj";
+            this.btnRacunaj.Size = new System.Drawing.Size(75, 23);
+            this.btnRacunaj.TabIndex = 0;
+            this.btnRacunaj.Text = "IZRAČUNAJ";
+            this.btnRacunaj.UseVisualStyleBackColor = true;
+            this.btnRacunaj.Click += new System.EventHandler(this.btnRacunaj_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "UNESITE PRVI BROJ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "UNESITE DRUGI BROJ";
+            // 
+            // lblRezultat
+            // 
+            this.lblRezultat.AutoSize = true;
+            this.lblRezultat.Location = new System.Drawing.Point(541, 66);
+            this.lblRezultat.Name = "lblRezultat";
+            this.lblRezultat.Size = new System.Drawing.Size(0, 13);
+            this.lblRezultat.TabIndex = 3;
+            // 
+            // txtPrviBroj
+            // 
+            this.txtPrviBroj.Location = new System.Drawing.Point(199, 27);
+            this.txtPrviBroj.Name = "txtPrviBroj";
+            this.txtPrviBroj.Size = new System.Drawing.Size(100, 20);
+            this.txtPrviBroj.TabIndex = 4;
+            // 
+            // txtDrugiBroj
+            // 
+            this.txtDrugiBroj.Location = new System.Drawing.Point(199, 58);
+            this.txtDrugiBroj.Name = "txtDrugiBroj";
+            this.txtDrugiBroj.Size = new System.Drawing.Size(100, 20);
+            this.txtDrugiBroj.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "VAŠA RIJEČ/REČENICA";
+            // 
+            // txtStringUnos
+            // 
+            this.txtStringUnos.Location = new System.Drawing.Point(199, 124);
+            this.txtStringUnos.Name = "txtStringUnos";
+            this.txtStringUnos.Size = new System.Drawing.Size(278, 20);
+            this.txtStringUnos.TabIndex = 7;
+            // 
+            // btnBrojacSlova
+            // 
+            this.btnBrojacSlova.Location = new System.Drawing.Point(544, 124);
+            this.btnBrojacSlova.Name = "btnBrojacSlova";
+            this.btnBrojacSlova.Size = new System.Drawing.Size(157, 23);
+            this.btnBrojacSlova.TabIndex = 8;
+            this.btnBrojacSlova.Text = "PREBROJI ZNAKOVE";
+            this.btnBrojacSlova.UseVisualStyleBackColor = true;
+            this.btnBrojacSlova.Click += new System.EventHandler(this.btnBrojacSlova_Click);
+            // 
+            // lblRezultatPrebrojavanja
+            // 
+            this.lblRezultatPrebrojavanja.AutoSize = true;
+            this.lblRezultatPrebrojavanja.Location = new System.Drawing.Point(196, 170);
+            this.lblRezultatPrebrojavanja.Name = "lblRezultatPrebrojavanja";
+            this.lblRezultatPrebrojavanja.Size = new System.Drawing.Size(0, 13);
+            this.lblRezultatPrebrojavanja.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(60, 223);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(128, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "DVA SLUČAJNA BROJA:";
+            // 
+            // lblPrviSlucBr
+            // 
+            this.lblPrviSlucBr.AutoSize = true;
+            this.lblPrviSlucBr.Location = new System.Drawing.Point(298, 223);
+            this.lblPrviSlucBr.Name = "lblPrviSlucBr";
+            this.lblPrviSlucBr.Size = new System.Drawing.Size(0, 13);
+            this.lblPrviSlucBr.TabIndex = 11;
+            // 
+            // lblDrugiSlucBr
+            // 
+            this.lblDrugiSlucBr.AutoSize = true;
+            this.lblDrugiSlucBr.Location = new System.Drawing.Point(375, 223);
+            this.lblDrugiSlucBr.Name = "lblDrugiSlucBr";
+            this.lblDrugiSlucBr.Size = new System.Drawing.Size(0, 13);
+            this.lblDrugiSlucBr.TabIndex = 12;
+            // 
+            // btnSlucBr
+            // 
+            this.btnSlucBr.Location = new System.Drawing.Point(544, 223);
+            this.btnSlucBr.Name = "btnSlucBr";
+            this.btnSlucBr.Size = new System.Drawing.Size(99, 23);
+            this.btnSlucBr.TabIndex = 13;
+            this.btnSlucBr.Text = "IZBACI";
+            this.btnSlucBr.UseVisualStyleBackColor = true;
+            this.btnSlucBr.Click += new System.EventHandler(this.btnSlucBr_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(60, 277);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(198, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "USPOREDBA DVA SLUČAJNA BROJA:";
+            // 
+            // btnSlucBrUsp
+            // 
+            this.btnSlucBrUsp.Location = new System.Drawing.Point(544, 277);
+            this.btnSlucBrUsp.Name = "btnSlucBrUsp";
+            this.btnSlucBrUsp.Size = new System.Drawing.Size(99, 23);
+            this.btnSlucBrUsp.TabIndex = 15;
+            this.btnSlucBrUsp.Text = "USPOREDI";
+            this.btnSlucBrUsp.UseVisualStyleBackColor = true;
+            this.btnSlucBrUsp.Click += new System.EventHandler(this.btnSlucBrUsp_Click);
+            // 
+            // lblRezUspSlucBr
+            // 
+            this.lblRezUspSlucBr.AutoSize = true;
+            this.lblRezUspSlucBr.Location = new System.Drawing.Point(298, 277);
+            this.lblRezUspSlucBr.Name = "lblRezUspSlucBr";
+            this.lblRezUspSlucBr.Size = new System.Drawing.Size(0, 13);
+            this.lblRezUspSlucBr.TabIndex = 16;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(852, 27);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(117, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "PRVI SLUČAJNI BROJ";
+            // 
+            // lblPrviSluc
+            // 
+            this.lblPrviSluc.AutoSize = true;
+            this.lblPrviSluc.Location = new System.Drawing.Point(894, 78);
+            this.lblPrviSluc.Name = "lblPrviSluc";
+            this.lblPrviSluc.Size = new System.Drawing.Size(0, 13);
+            this.lblPrviSluc.TabIndex = 18;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(1007, 27);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(127, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "DRUGI SLUČAJNI BROJ";
+            // 
+            // lblDrugiSluc
+            // 
+            this.lblDrugiSluc.AutoSize = true;
+            this.lblDrugiSluc.Location = new System.Drawing.Point(1055, 78);
+            this.lblDrugiSluc.Name = "lblDrugiSluc";
+            this.lblDrugiSluc.Size = new System.Drawing.Size(0, 13);
+            this.lblDrugiSluc.TabIndex = 20;
+            // 
+            // btnPokreniSluc
+            // 
+            this.btnPokreniSluc.Location = new System.Drawing.Point(1170, 55);
+            this.btnPokreniSluc.Name = "btnPokreniSluc";
+            this.btnPokreniSluc.Size = new System.Drawing.Size(75, 23);
+            this.btnPokreniSluc.TabIndex = 21;
+            this.btnPokreniSluc.Text = "POKRENI";
+            this.btnPokreniSluc.UseVisualStyleBackColor = true;
+            this.btnPokreniSluc.Click += new System.EventHandler(this.btnPokreniSluc_Click);
+            // 
+            // lblIspisZadatka
+            // 
+            this.lblIspisZadatka.AutoSize = true;
+            this.lblIspisZadatka.Location = new System.Drawing.Point(952, 203);
+            this.lblIspisZadatka.Name = "lblIspisZadatka";
+            this.lblIspisZadatka.Size = new System.Drawing.Size(0, 13);
+            this.lblIspisZadatka.TabIndex = 22;
+            // 
+            // btnPridruzivanje
+            // 
+            this.btnPridruzivanje.Location = new System.Drawing.Point(1130, 155);
+            this.btnPridruzivanje.Name = "btnPridruzivanje";
+            this.btnPridruzivanje.Size = new System.Drawing.Size(115, 23);
+            this.btnPridruzivanje.TabIndex = 23;
+            this.btnPridruzivanje.Text = "PRIDRUŽIVANJE";
+            this.btnPridruzivanje.UseVisualStyleBackColor = true;
+            // 
+            // txtUnosBr
+            // 
+            this.txtUnosBr.Location = new System.Drawing.Point(955, 157);
+            this.txtUnosBr.Name = "txtUnosBr";
+            this.txtUnosBr.Size = new System.Drawing.Size(100, 20);
+            this.txtUnosBr.TabIndex = 24;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(868, 161);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(62, 13);
+            this.label9.TabIndex = 25;
+            this.label9.Text = "VAŠ BROJ:";
+            // 
+            // btnUsporedjivanje
+            // 
+            this.btnUsporedjivanje.Location = new System.Drawing.Point(1130, 192);
+            this.btnUsporedjivanje.Name = "btnUsporedjivanje";
+            this.btnUsporedjivanje.Size = new System.Drawing.Size(115, 23);
+            this.btnUsporedjivanje.TabIndex = 26;
+            this.btnUsporedjivanje.Text = "USPOREĐIVANJE";
+            this.btnUsporedjivanje.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1288, 450);
+            this.Controls.Add(this.btnUsporedjivanje);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtUnosBr);
+            this.Controls.Add(this.btnPridruzivanje);
+            this.Controls.Add(this.lblIspisZadatka);
+            this.Controls.Add(this.btnPokreniSluc);
+            this.Controls.Add(this.lblDrugiSluc);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblPrviSluc);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblRezUspSlucBr);
+            this.Controls.Add(this.btnSlucBrUsp);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnSlucBr);
+            this.Controls.Add(this.lblDrugiSlucBr);
+            this.Controls.Add(this.lblPrviSlucBr);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblRezultatPrebrojavanja);
+            this.Controls.Add(this.btnBrojacSlova);
+            this.Controls.Add(this.txtStringUnos);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtDrugiBroj);
+            this.Controls.Add(this.txtPrviBroj);
+            this.Controls.Add(this.lblRezultat);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRacunaj);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRacunaj;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblRezultat;
+        private System.Windows.Forms.TextBox txtPrviBroj;
+        private System.Windows.Forms.TextBox txtDrugiBroj;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtStringUnos;
+        private System.Windows.Forms.Button btnBrojacSlova;
+        private System.Windows.Forms.Label lblRezultatPrebrojavanja;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblPrviSlucBr;
+        private System.Windows.Forms.Label lblDrugiSlucBr;
+        private System.Windows.Forms.Button btnSlucBr;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnSlucBrUsp;
+        private System.Windows.Forms.Label lblRezUspSlucBr;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblPrviSluc;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblDrugiSluc;
+        private System.Windows.Forms.Button btnPokreniSluc;
+        private System.Windows.Forms.Label lblIspisZadatka;
+        private System.Windows.Forms.Button btnPridruzivanje;
+        private System.Windows.Forms.TextBox txtUnosBr;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnUsporedjivanje;
+    }
+}
+
---
+++ vjezbanje1/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vjezbanje1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRacunaj_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(txtPrviBroj.Text);
+            int b = Convert.ToInt32(txtDrugiBroj.Text);
+            int c = a + b;
+
+            lblRezultat.Text = c.ToString();
+
+            
+        }
+
+        private void btnBrojacSlova_Click(object sender, EventArgs e)
+        {
+            string rijec = txtStringUnos.Text;
+            int brojZnakova = rijec.Length;
+            int brojSamoglasnika = 0;
+
+            for (int i = 0; i < brojZnakova; i++)
+            {
+                if (rijec[i] == 'A' || rijec[i] == 'a' || rijec[i] == 'E' || rijec[i] == 'e' || rijec[i] == 'I' || rijec[i] == 'i' || rijec[i] == 'O' || rijec[i] == 'o' || rijec[i] == 'U' || rijec[i] == 'u')
+                {
+                    brojSamoglasnika++;
+                }
+            }
+
+            lblRezultatPrebrojavanja.Text = "Broj znakova u vašoj riječi/rečenici, uključujući razmak, je: " + brojZnakova + "\n" + "Broj samoglasnika u riječi/rečenici \"" + txtStringUnos.Text + "\" je: " + brojSamoglasnika.ToString();
+        }
+
+        private void btnSlucBr_Click(object sender, EventArgs e)
+        {
+            Random randomDvaBr = new Random();
+            int[] nizrandomDvaBr = new int[2];
+
+            for (int i = 0; i < 2; i++)
+            {
+                nizrandomDvaBr[i] = randomDvaBr.Next(0, 50);
+            }
+            // lblPrviSlucBr.Text = nizrandomDvaBr[0].ToString(); 
+            // lblDrugiSlucBr.Text = nizrandomDvaBr[1].ToString();
+
+            int a = Convert.ToInt32(lblPrviSlucBr.Text = nizrandomDvaBr[0].ToString());
+            int b = Convert.ToInt32(lblDrugiSlucBr.Text = nizrandomDvaBr[1].ToString());
+        }
+
+        private void btnSlucBrUsp_Click(object sender, EventArgs e)
+        {
+            Random randomDvaBr = new Random();
+            int[] nizrandomDvaBr = new int[2];
+
+            for (int i = 0; i < 2; i++)
+            {
+                nizrandomDvaBr[i] = randomDvaBr.Next(0, 51);
+            }
+            int a = Convert.ToInt32(lblPrviSlucBr.Text = nizrandomDvaBr[0].ToString());
+            int b = Convert.ToInt32(lblDrugiSlucBr.Text = nizrandomDvaBr[1].ToString()); 
+
+            
+
+            if (a > b)
+            {
+                lblRezUspSlucBr.Text = "Veći je broj: " + a;
+            }
+            else if (a < b)
+            {
+                lblRezUspSlucBr.Text = "Veći je broj: " + b;
+            }
+            else
+            {
+                lblRezUspSlucBr.Text = "Brojevi " + a + " i " + b + " su jednaki.";
+            }
+        }
+
+        private void btnPokreniSluc_Click(object sender, EventArgs e)
+        {
+            Random rndBrojevi = new Random();
+            int[] rndNizBrojeva = new int[2];
+
+            for (int i = 0; i < 2; i++)
+            {
+                rndNizBrojeva[i] = rndBrojevi.Next(0, 51);
+            }
+            lblPrviSluc.Text = rndNizBrojeva[0].ToString();
+            lblDrugiSluc.Text = rndNizBrojeva[1].ToString();
+
+        }
+    }
+}
